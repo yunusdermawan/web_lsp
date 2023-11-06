136a679a3186e7ed65930315cd7e7b8798643488
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-11-07 00:04:37 +0700

    adding large BP

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -44,7 +44,7 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className='lg:h-[500px] relative'>
+            <div className='lg:h-[490px] relative'>
                 <div className='absolute grid gap-8 grid-cols-2 z-30 lg:mt-6 lg:py-6 lg:px-8 bg-[#F2F0F4] rounded-3xl'>
                     <div className='flex justify-center items-center'>
                         <img className='w-[80%]' src={hero} alt="" />
@@ -56,112 +56,112 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className='lg:mt-4'>
+            <div className='lg:mt-0'>
                 <div className='flex justify-center uppercase lg:text-[2rem] text-white'><h1>Skema LSP P1 digital kreatif solusi</h1></div>
-                <div className='lg:mt-12 lg:py-8 lg:px-10 bg-[#F2F0F4] w-full font-sans lg:text-xl lg:font-semibold text-gray-500'>
-                    <div className='flex mb-4'>
-                        <span className='lg:w-16'>1</span>
-                        <span className='w-[650px]'>Ahli Fluida Pengeboran Migas dan Panas Bumi</span>
-                        <div><ul className='list-disc uppercase'><li>Teknik Perminyakan</li></ul></div>
+                <div className='lg:mt-2 lg:py-2 lg:px-6 bg-[#F2F0F4] w-full font-sans lg:text-xl lg:font-semibold text-gray-500'>
+                    <div className='flex lg:mb-1'>
+                        <span className='lg:w-16 lg:text-[0.8rem]'>1</span>
+                        <span className='lg:w-[450px] lg:text-[0.8rem]'>Ahli Fluida Pengeboran Migas dan Panas Bumi</span>
+                        <div ><ul className='list-disc uppercase lg:text-[0.8rem]'><li>Teknik Perminyakan</li></ul></div>
                     </div>
-                    <div className='flex border-t-4 mb-4'>
-                        <span className='w-16 mt-4'>2</span>
-                        <span className='w-[650px] mt-4'>Ahli Keselamatan dan Kesehatan Kerja</span>
-                        <div className='mt-4'>
+                    <div className='flex lg:border-t-2 lg:mb-1'>
+                        <span className='lg:w-16 lg:text-[0.8rem] lg:mt-2'>2</span>
+                        <span className='lg:w-[450px] lg:text-[0.8rem] lg:mt-2'>Ahli Keselamatan dan Kesehatan Kerja</span>
+                        <div className='lg:mt-1'>
                             <ul className='list-disc uppercase'>
-                                <li className='mb-2'>Teknik Lingkungan</li>
-                                <li className='mb-2'>Teknik Kimia</li>
-                                <li className='mb-2'>Teknik Industri</li>
-                                <li className='mb-2'>Teknik Perminyakan</li>
+                                <li className='lg:text-[0.8rem] lg:-mb-2'>Teknik Lingkungan</li>
+                                <li className='lg:text-[0.8rem] lg:-mb-2'>Teknik Kimia</li>
+                                <li className='lg:text-[0.8rem] lg:-mb-2'>Teknik Industri</li>
+                                <li className='lg:text-[0.8rem] lg:-mb-2'>Teknik Perminyakan</li>
                             </ul>
                         </div>
                     </div>
-                    <div className='flex border-t-4 mb-4'>
-                        <span className='w-16 mt-4'>3</span>
-                        <span className='w-[650px] mt-4'>Analisis Kemananan Siber</span>
-                        <div className='mt-4'>
-                            <ul className='list-disc uppercase'>
+                    <div className='flex lg:border-t-2 lg:mb-1'>
+                        <span className='lg:w-16 lg:text-[0.8rem] lg:mt-2'>3</span>
+                        <span className='lg:w-[450px] lg:text-[0.8rem] lg:mt-2'>Analisis Kemananan Siber</span>
+                        <div className='lg:mt-1' >
+                            <ul className='list-disc uppercase lg:text-[0.8rem]'>
                                 <li>Informatika</li>
                             </ul>
                         </div>
                     </div>
-                    <div className='flex border-t-4 mb-4'>
-                        <span className='w-16 mt-4'>4</span>
-                        <span className='w-[650px] mt-4'>Asisten Produser Televisi</span>
-                        <div className='mt-4'>
-                            <ul className='list-disc uppercase'>
+                    <div className='flex lg:border-t-2 lg:mb-1'>
+                        <span className='lg:w-16 lg:text-[0.8rem] lg:mt-2'>4</span>
+                        <span className='lg:w-[450px] lg:text-[0.8rem] lg:mt-2'>Asisten Produser Televisi</span>
+                        <div className='lg:mt-1' >
+                            <ul className='list-disc uppercase lg:text-[0.8rem]'>
                                 <li>Ilmu Komunikasi</li>
                             </ul>
                         </div>
                     </div>
-                    <div className='flex border-t-4 mb-4'>
-                        <span className='w-16 mt-4'>5</span>
-                        <span className='w-[650px] mt-4'>Desainer Grafis Madya</span>
-                        <div className='mt-4'>
-                            <ul className='list-disc uppercase'>
+                    <div className='flex lg:border-t-2 lg:mb-1'>
+                        <span className='lg:w-16 lg:text-[0.8rem] lg:mt-2'>5</span>
+                        <span className='lg:w-[450px] lg:text-[0.8rem] lg:mt-2'>Desainer Grafis Madya</span>
+                        <div className='lg:mt-1' >
+                            <ul className='list-disc uppercase lg:text-[0.8rem]'>
                                 <li>Ilmu Komunikasi</li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div className='flex justify-center text-white mt-4'>
-                    <span>Lihat: </span>
-                    <span className='font-bold'>23</span>
-                    <span><Link to='#'> skema lainya...</Link></span>
+                <div className='flex justify-center text-white lg:mt-4'>
+                    <span className='lg:text-[0.8rem]'>Lihat: </span>
+                    <span className='font-bold lg:text-[0.8rem] lg:mx-2'>23</span>
+                    <span className='lg:text-[0.8rem]'><Link to='#'>skema lainnya...</Link></span>
                 </div>
             </div>
-            <div className='mt-44'>
-                <div className='relative w-[70%] mx-auto'>
+            <div className='lg:mt-24'>
+                <div className='relative lg:w-[80%] lg:mx-auto'>
                     <div className='flex flex-col justify-center'>
                         <div className='flex justify-center uppercase'>
-                            <div className='flex text-4xl'><h1 className='me-3 text-[#FF7F00]'>Persyaratan</h1><h1>Umum</h1></div>
+                            <div className='flex lg:text-[2rem]'><h1 className='lg:me-3 text-[#FF7F00]'>Persyaratan</h1><h1>Umum</h1></div>
                         </div>
-                        <div className='mt-10'>
-                            <ul className='list-decimal'>
-                                <li className='mb-2'>Form APL 01 dan APL 02 tidak perlu diprint , Save as menjadi PDF.</li>
-                                <li className='mb-2'>Tanda tangan asesi menggunakan digital.</li>
-                                <li className='mb-2'>Perhatikan pada Form APL 02, arti kode K = Kompeten dan BK = Belum Kompeten.</li>
-                                <li className='mb-2'>Dokumen yang digunakan sebagai persyaratan pendaftaran, usahakan sudah terdapat tanda tangan pengesahan dari pihak bersangkutan, seperti KHS, Sertifikat,dll.</li>
+                        <div className='lg:mt-1'>
+                            <ul className='list-decimal text-[0.7rem]'>
+                                <li className='lg:mb-0'>Form APL 01 dan APL 02 tidak perlu diprint , Save as menjadi PDF.</li>
+                                <li className='lg:mb-0'>Tanda tangan asesi menggunakan digital.</li>
+                                <li className='lg:mb-0'>Perhatikan pada Form APL 02, arti kode K = Kompeten dan BK = Belum Kompeten.</li>
+                                <li className='lg:mb-0'>Dokumen yang digunakan sebagai persyaratan pendaftaran, usahakan sudah terdapat tanda tangan pengesahan dari pihak bersangkutan, seperti KHS, Sertifikat,dll.</li>
                             </ul>
                         </div>
                     </div>
-                    <div className='absolute right-[-320px] top-12'>
-                        <img className='w-[250px]' src={reg_section4} alt="" />
+                    <div className='absolute lg:right-[-160px] lg:top-8'>
+                        <img className='lg:w-[150px]' src={reg_section4} alt="" />
                     </div>
                 </div>
-                <div className='mt-10'>
+                <div className='lg:mt-6'>
                     <div className='flex justify-center uppercase'>
-                        <div className='flex text-4xl'><h1 className='me-3 text-[#FF7F00]'>Tahapan</h1><h1>Untuk Mendaftar</h1></div>
+                        <div className='flex lg:text-[2rem]'><h1 className='lg:me-3 text-[#FF7F00]'>Tahapan</h1><h1>Untuk Mendaftar</h1></div>
                     </div>
-                    <div className='flex justify-center items-center mt-10'>
-                        <div className='rounded-3xl rounded-bl-none border-4 shadow-[inset_0px_0px_20px_0px_#ccc] bg-white p-4 me-4'>
-                            <img src={reg_section1} alt="" />
+                    <div className='flex justify-center items-center lg:mt-2'>
+                        <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:p-2 lg:me-2'>
+                            <img className='lg:w-12' src={reg_section1} alt="" />
                         </div>
-                        <div className='self-start me-4'>
+                        <div className='lg:w-40 self-start lg:me-2'>
                             <img src={reg_arr1} alt="" />
                         </div>
-                        <div className='rounded-3xl rounded-bl-none border-4 shadow-[inset_0px_0px_20px_0px_#ccc] bg-white py-6 px-7 me-4'>
-                            <img className='w-12' src={reg_section2} alt="" />
+                        <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:py-3 lg:px-4 lg:me-2'>
+                            <img className='w-8' src={reg_section2} alt="" />
                         </div>
-                        <div className='self-end me-4'>
+                        <div className='lg:w-40 self-end lg:me-2'>
                             <img src={reg_arr2} alt="" />
                         </div>
-                        <div className='rounded-3xl rounded-bl-none border-4 shadow-[inset_0px_0px_20px_0px_#ccc] bg-white p-5'>
-                            <img src={reg_section3} alt="" />
+                        <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:p-2'>
+                            <img className='lg:w-12' src={reg_section3} alt="" />
                         </div>
                     </div>
                     <div className='grid grid-cols-3'>
-                        <div className='text-center w-[80%] mt-4'>
-                            <h6 className='text-warning font-bold mb-1'>Your Content Goes Here</h6>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        <div className='text-center lg:w-[80%] lg:mt-4'>
+                            <h6 className='text-warning font-bold lg:mb-1 lg:text-[0.8rem]'>Your Content Goes Here</h6>
+                            <p className='lg:text-[0.8rem]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
                         <div className='text-center w-[80%] mx-auto mt-4'>
-                            <h6 className='text-warning font-bold mb-1'>Your Content Goes Here</h6>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <h6 className='text-warning font-bold lg:mb-1 lg:text-[0.8rem]'>Your Content Goes Here</h6>
+                            <p className='lg:text-[0.8rem]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
                         <div className='text-center w-[80%] mx-auto mt-4'>
-                            <h6 className='text-warning font-bold mb-1'>Your Content Goes Here</h6>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <h6 className='text-warning font-bold lg:mb-1 lg:text-[0.8rem]'>Your Content Goes Here</h6>
+                            <p className='lg:text-[0.8rem]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
                     </div>
                 </div>
