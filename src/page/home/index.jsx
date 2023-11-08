62ce015dcb49a59b403120b4436cf7138f2e1554
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-11-08 08:55:38 +0700

    Fixing tab BP - 768px

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -19,7 +19,7 @@ import hero from '../../asset/hero_section.png'
 
 function Home() {
   return (
-    <div style={{backgroundImage: `url(${background})`, height: '100%', width: '100vw'}} className='container bg-no-repeat md:bg-cover lg:bg-contain xl:bg-cover overflow-hidden'>
+    <div style={{backgroundImage: `url(${background})`, height: '100%', width: '100vw'}} className='container bg-no-repeat bg-cover lg:bg-contain xl:bg-cover overflow-hidden'>
 
         <header>
             <div className='md:pb-10 lg:pb-16'>
@@ -29,6 +29,7 @@ function Home() {
         </header>
 
         <main className='lg:px-24 min-h-screen'>
+            {/* Hero section */}
             <div className='grid grid-cols-2' id='hero'>
                 <div className='lg:w-80 xl:w-96'>
                     <h5 className='text-info font-semibold font-azeret md:text-[0.8rem] lg:text-xs xl:text-sm lg:mb-3'>Tentang Digital Kreatif Solusi</h5>
@@ -43,29 +44,40 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className='md:h-[30rem] lg:h-[30.625rem] xl:h-[37.5rem] relative'>
-                <div className='absolute grid gap-8 grid-cols-2 z-30 md:mt-4 lg:mt-6 md:py-4 lg:py-6 xl:py-10 lg:px-8 bg-[#F2F0F4] rounded-3xl'>
-                    <div className='flex justify-center items-center'>
+            {/* Schema section */}
+            <div className='md:h-[50rem] lg:h-[30.625rem] xl:h-[37.5rem] relative'>
+                <div className='absolute grid gap-8 grid-cols-1 lg:grid-cols-2 z-30 md:mt-4 lg:mt-6 md:py-4 lg:py-6 xl:py-10 lg:px-8 bg-[#F2F0F4] rounded-3xl'>
+                    {/* Tab BP - 768px */}
+                    <div className='md:flex flex-col justify-center items-center lg:hidden px-16'>
+                        <div className='text-center'>
+                            <h1 className='md:text-[1.5rem] lg:text-3xl xl:text-4xl md:mb-2 lg:mb-3'>Skema Sertifikasi</h1>
+                            <p className='lg:text-[0.6rem] xl:text-xs md:mb-2 lg:mb-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum auctor odio eget metus gravida, at feugiat erat consequat.</p>
+                            <p className='lg:text-[0.6rem] xl:text-xs text-gray-400 mb-4'>ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum auctor odio eget metus gravida, at feugiat erat consequat.</p>
+                        </div>
                         <img className='w-[80%]' src={hero} alt="" />
                     </div>
-                    <div>
+                    {/* Large BP - 1204px */}
+                    <div className='md:hidden lg:flex flex-col justify-center items-center'>
+                        <img className='w-[80%]' src={hero} alt="" />
+                    </div>
+                    <div className='hidden lg:block'>
                         <h1 className='md:text-[1.5rem] lg:text-3xl xl:text-4xl lg:mb-3'>Skema Sertifikasi</h1>
                         <p className='lg:text-[0.6rem] xl:text-xs lg:mb-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum auctor odio eget metus gravida, at feugiat erat consequat.</p>
                         <p className='lg:text-[0.6rem] xl:text-xs text-gray-400'>ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum auctor odio eget metus gravida, at feugiat erat consequat.</p>
                     </div>
                 </div>
             </div>
-            <div className='lg:mt-0 xl:mt-4'>
-                <div className='flex justify-center uppercase lg:text-[2rem] text-white'><h1>Skema LSP P1 digital kreatif solusi</h1></div>
-                <div className='lg:mt-2 xl:mt-6 lg:py-2 xl:py-4 lg:px-6 bg-[#F2F0F4] w-full font-sans lg:text-xl lg:font-semibold text-gray-500'>
+            <div className=' md:bg-[#F86D42] lg:bg-none rounded-xl lg:mt-0 xl:mt-4 md:p-4 lg:p-0'>
+                <div className='flex justify-center uppercase md:text-[1.5rem] lg:text-[2rem] md:font-bold text-white mb-2'><h1>Skema LSP P1 digital kreatif solusi</h1></div>
+                <div className='lg:mt-2 xl:mt-6 lg:py-2 xl:py-4 lg:px-6 lg:bg-[#F2F0F4] w-full font-sans lg:text-xl lg:font-semibold md:text-white lg:text-gray-500'>
                     <div className='flex lg:mb-1 xl:mb-3'>
-                        <span className='lg:w-16 lg:text-[0.8rem] xl:text-sm'>1</span>
-                        <span className='lg:w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm'>Ahli Fluida Pengeboran Migas dan Panas Bumi</span>
+                        <span className='w-16 lg:text-[0.8rem] xl:text-sm'>1</span>
+                        <span className='w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm'>Ahli Fluida Pengeboran Migas dan Panas Bumi</span>
                         <div ><ul className='list-disc uppercase lg:text-[0.8rem] xl:text-sm'><li>Teknik Perminyakan</li></ul></div>
                     </div>
                     <div className='flex lg:border-t-2 xl:border-t-[0.185rem] lg:mb-1 xl:mb-3'>
-                        <span className='lg:w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>2</span>
-                        <span className='lg:w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Ahli Keselamatan dan Kesehatan Kerja</span>
+                        <span className='w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>2</span>
+                        <span className='w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Ahli Keselamatan dan Kesehatan Kerja</span>
                         <div className='lg:mt-1 xl:mt-2'>
                             <ul className='list-disc uppercase'>
                                 <li className='lg:text-[0.8rem] xl:text-sm lg:-mb-2 xl:mb-0'>Teknik Lingkungan</li>
@@ -76,8 +88,8 @@ function Home() {
                         </div>
                     </div>
                     <div className='flex lg:border-t-2 xl:border-t-[0.185rem] lg:mb-1 xl:mb-3'>
-                        <span className='lg:w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>3</span>
-                        <span className='lg:w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Analisis Kemananan Siber</span>
+                        <span className='w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>3</span>
+                        <span className='w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Analisis Kemananan Siber</span>
                         <div className='lg:mt-1 xl:mt-2' >
                             <ul className='list-disc uppercase lg:text-[0.8rem] xl:text-sm'>
                                 <li>Informatika</li>
@@ -85,8 +97,8 @@ function Home() {
                         </div>
                     </div>
                     <div className='flex lg:border-t-2 xl:border-t-[0.185rem] lg:mb-1 xl:mb-3'>
-                        <span className='lg:w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>4</span>
-                        <span className='lg:w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Asisten Produser Televisi</span>
+                        <span className='w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>4</span>
+                        <span className='w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Asisten Produser Televisi</span>
                         <div className='lg:mt-1 xl:mt-2' >
                             <ul className='list-disc uppercase lg:text-[0.8rem] xl:text-sm'>
                                 <li>Ilmu Komunikasi</li>
@@ -94,8 +106,8 @@ function Home() {
                         </div>
                     </div>
                     <div className='flex lg:border-t-2 xl:border-t-[0.185rem] lg:mb-1 xl:mb-3'>
-                        <span className='lg:w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>5</span>
-                        <span className='lg:w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Desainer Grafis Madya</span>
+                        <span className='w-16 lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>5</span>
+                        <span className='w-[28.125rem] xl:w-[43.75rem] lg:text-[0.8rem] xl:text-sm lg:mt-2 xl:mt-3'>Desainer Grafis Madya</span>
                         <div className='lg:mt-1 xl:mt-2' >
                             <ul className='list-disc uppercase lg:text-[0.8rem] xl:text-sm'>
                                 <li>Ilmu Komunikasi</li>
@@ -103,19 +115,19 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className='flex justify-center text-white lg:mt-4'>
-                    <span className='lg:text-[0.8rem]'>Lihat: </span>
-                    <span className='font-bold lg:text-[0.8rem] lg:mx-2'>23</span>
-                    <span className='lg:text-[0.8rem]'><Link to='#'>skema lainnya...</Link></span>
+                <div className='flex justify-center text-white mt-4'>
+                    <span className='md:text-[0.75rem] lg:text-[0.8rem]'>Lihat: </span>
+                    <span className='font-bold md:text-[0.75rem] lg:text-[0.8rem] lg:mx-2'>23</span>
+                    <span className='md:text-[0.75rem] lg:text-[0.8rem]'><Link to='#'>skema lainnya...</Link></span>
                 </div>
             </div>
-            <div className='lg:mt-24 xl:mt-40'>
-                <div className='relative lg:w-[80%] lg:mx-auto'>
+            <div className='md:mt-8 lg:mt-24 xl:mt-40'>
+                <div className='relative lg:w-[80%] lg:mx-auto md:px-8'>
                     <div className='flex flex-col justify-center'>
                         <div className='flex justify-center uppercase'>
-                            <div className='flex lg:text-[2rem]'><h1 className='lg:me-3 text-[#FF7F00]'>Persyaratan</h1><h1>Umum</h1></div>
+                            <div className='flex lg:text-[2rem]'><h1 className='md:me-3 text-[#FF7F00]'>Persyaratan</h1><h1>Umum</h1></div>
                         </div>
-                        <div className='lg:mt-1 xl:mt-3'>
+                        <div className='md:mt-2 lg:mt-1 xl:mt-3'>
                             <ul className='list-decimal text-[0.7rem]'>
                                 <li className='lg:mb-0 xl:mb-1'>Form APL 01 dan APL 02 tidak perlu diprint , Save as menjadi PDF.</li>
                                 <li className='lg:mb-0 xl:mb-1'>Tanda tangan asesi menggunakan digital.</li>
@@ -124,29 +136,29 @@ function Home() {
                             </ul>
                         </div>
                     </div>
-                    <div className='absolute lg:right-[-10rem] lg:top-8'>
-                        <img className='lg:w-[9.375rem]' src={reg_section4} alt="" />
+                    <div className='absolute md:right-16 lg:right-[-10rem] md:top-0 lg:top-8'>
+                        <img className='md:w-[7rem] lg:w-[9.375rem]' src={reg_section4} alt="" />
                     </div>
                 </div>
-                <div className='lg:mt-6'>
+                <div className='md:mt-6'>
                     <div className='flex justify-center uppercase'>
-                        <div className='flex lg:text-[2rem]'><h1 className='lg:me-3 text-[#FF7F00]'>Tahapan</h1><h1>Untuk Mendaftar</h1></div>
+                        <div className='flex lg:text-[2rem]'><h1 className='md:me-2 lg:me-3 text-[#FF7F00]'>Tahapan</h1><h1>Untuk Mendaftar</h1></div>
                     </div>
-                    <div className='flex justify-center items-center lg:mt-2'>
+                    <div className='flex justify-center items-center md:mt-2'>
                         <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:p-2 lg:me-2'>
-                            <img className='lg:w-12' src={reg_section1} alt="" />
+                            <img className='md:w-10 lg:w-12' src={reg_section1} alt="" />
                         </div>
-                        <div className='lg:w-40 xl:w-60 self-start lg:me-2'>
+                        <div className='md:w-28 lg:w-40 xl:w-60 self-start lg:me-2'>
                             <img src={reg_arr1} alt="" />
                         </div>
                         <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:py-3 lg:px-4 lg:me-2'>
                             <img className='w-8' src={reg_section2} alt="" />
                         </div>
-                        <div className='lg:w-40 xl:w-60 self-end lg:me-2'>
+                        <div className='md:w-28 lg:w-40 xl:w-60 self-end lg:me-2'>
                             <img src={reg_arr2} alt="" />
                         </div>
                         <div className='lg:rounded-2xl lg:rounded-bl-none lg:border-4 shadow-[inset_0px_0px_10px_0px_#ccc] bg-white lg:p-2'>
-                            <img className='lg:w-12' src={reg_section3} alt="" />
+                            <img className='md:w-10 lg:w-12' src={reg_section3} alt="" />
                         </div>
                     </div>
                     <div className='grid grid-cols-3'>
